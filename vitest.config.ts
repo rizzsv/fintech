6408b0d7f60e6b488d1d10fee351b9060441981a
rizzsv <rizq.syafriano@gmail.com>
2026-07-04 15:08:34 +0700

    fix: more bug in ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vitest/config";
-import path from "node:path";
 
 export default defineConfig({
   test: {
-    globals: true,
-    environment: "node",
-    setupFiles: [
-      path.resolve(__dirname, "test/setup.ts"),
+    exclude: [
+      "node_modules",
+      "dist",
+
+      "test/integration/ledger/**",
+      "test/integration/transaction/**",
+      "test/unit/transaction/**",
     ],
   },
 });
